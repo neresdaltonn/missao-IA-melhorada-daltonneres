3e782fb55e8c738986d432d495a99ca20ad72b13
Dalton José Neres <145723466+neresdaltonn@users.noreply.github.com>
2025-11-25 12:02:39 -0300

    upload

--- aleatorio.js
+++ aleatorio.js
@@ -1,3 +1,5 @@
+const nomes = ["Bruna", "Dalton", "João", "Junior", "Luiz", "José", "Carlos" ]
+
 export function aleatorio(lista){
     const posicao = Math.floor(Math.random()*lista.length);
     return lista[posicao];
